60332e88770d57c0ae5fef5705dabec4ff27d0ca
Onur Yılmazer <onur.yilmazer1@gmail.com>
2022-05-15 14:24:42 +0200

    added task 3.2

---
+++ Week3/3.2 Zahlenraten.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+void askForBoundaries(int& lowerBound, int& upperBound);
+bool askYNQeustion(const string &question);
+int tryToGuessNumber(int lowerBound, int upperBound);
+
+
+int main() {
+    int lowerBound, upperBound;
+    askForBoundaries(lowerBound, upperBound);    
+
+    int number = tryToGuessNumber(lowerBound, upperBound);
+    return 0;
+
+}
+
+void askForBoundaries(int& lowerBound, int& upperBound) {
+    cout << "Enter the lower and upper bounds, both inclusive:\n";
+    cin >> lowerBound;
+    cin >> upperBound;
+    cin.ignore();
+
+    if (lowerBound > upperBound) {
+        int temp = lowerBound;
+        lowerBound = upperBound;
+        upperBound = temp;
+    }
+}
+
+bool askYNQeustion(const string &question) {
+    cout << question << " yes/no\n";
+    string answer;
+    getline(cin, answer);
+    bool answerIsCorrect = answer == "yes";
+
+    return answerIsCorrect;
+}
+
+int tryToGuessNumber(int lowerBound, int upperBound) {
+    int lengthOfInterval = upperBound - lowerBound;
+    int middleIndex = lengthOfInterval / 2;
+    int middleNumber = lowerBound + middleIndex;
+
+    string askIfMiddle = "Is your number " + to_string(middleNumber) + "?";
+    string askIfLower = "Is your number between " + to_string(lowerBound) + " and " + to_string(middleNumber-1) + " inclusive?";
+
+
+    if(askYNQeustion(askIfMiddle)) {
+        cout << "Found it! " + to_string(middleNumber);
+        return middleNumber;
+    }
+    else if (lengthOfInterval == 1) {
+        cout << "The only other possibility is " + to_string(middleNumber+1);
+        return middleNumber+1;
+    }
+    else if (askYNQeustion(askIfLower)) {
+        return tryToGuessNumber(lowerBound, middleNumber-1);
+    }
+    else {
+        return tryToGuessNumber(middleNumber + 1, upperBound);
+    }
+}
